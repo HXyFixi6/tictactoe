144fa13ff4ce5dbc97c6a989bb13e9d1d6ed41bf
HXyFixi6 <lucas@fuentesg.net>
2026-05-25 15:42:27 +0200

    [ADD] random bot

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Board from './components/Board';
 import Setup from './components/Setup';
 import './App.css';
@@ -7,6 +7,7 @@ function App() {
   const [playerSymbol, setPlayerSymbol] = useState(null);
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isHumanTurn, setIsHumanTurn] = useState(true);
+  const botSymbol = playerSymbol === 'X' ? 'O' : 'X';
 
   const handleSquareClick = (index) => {
     if (squares[index] !== null || !isHumanTurn) {
@@ -17,7 +18,30 @@ function App() {
     nextSquares[index] = playerSymbol;
     
     setSquares(nextSquares);
+    setIsHumanTurn(false);
   };
+
+  useEffect(() => {
+    if (isHumanTurn || !playerSymbol) return;
+
+    const emptyIndexes = squares
+      .map((square, index) => (square === null ? index : null))
+      .filter((val) => val !== null);
+
+    if (emptyIndexes.length > 0) {
+      const timer = setTimeout(() => {
+        const randomIndex = emptyIndexes[Math.floor(Math.random() * emptyIndexes.length)];
+        
+        const nextSquares = [...squares];
+        nextSquares[randomIndex] = botSymbol;
+        
+        setSquares(nextSquares);
+        setIsHumanTurn(true); 
+      }, 500); 
+
+      return () => clearTimeout(timer);
+    }
+  }, [isHumanTurn, squares, playerSymbol, botSymbol]);
 
   const handleReset = () => {
     setSquares(Array(9).fill(null));
