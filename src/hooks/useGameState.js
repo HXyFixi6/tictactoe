2aa3302c8f90643abd68842d6b6108eb2a3255fa
HXyFixi6 <lucas@fuentesg.net>
2026-05-26 17:25:46 +0200

    [RF] hook name convention

--- src/hooks/useGameState.js
+++ src/hooks/useGameState.js
@@ -3,7 +3,7 @@ import { checkWinner } from '../utils/gameLogic';
 
 export const GRID_SIZE = 3;
 
-export const gameState = () => {
+export const useGameState = () => {
   const [playerSymbol, setPlayerSymbol] = useState(null);
   const [squares, setSquares] = useState(Array(GRID_SIZE * GRID_SIZE).fill(null));
   const [isHumanTurn, setIsHumanTurn] = useState(true);
